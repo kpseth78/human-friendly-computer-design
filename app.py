77914d7b9351dcc3ea727b26ddb52b994dfa3323
kpseth78 <69121003+kpseth78@users.noreply.github.com>
2021-02-12 00:11:27 -0800

    Adding Posts Front End

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ export FLASK_ENV=development
 flask run
 '''
 from flask_restful import Api
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from flask_cors import CORS
 
 import db
@@ -17,9 +17,21 @@ db.init_database_connection(app)
 api = Api(app)
 
 # connect your routes to your app:
+# @app.route('/')
+# def home_page():
+#     return 'This is your API Homepage'
+
 @app.route('/')
-def home_page():
-    return 'This is your API Homepage'
+def list_posts():
+    return render_template('get-posts.html')
+
+@app.route('/add-post/')
+def create_post():
+    return render_template('create-post.html')
+
+@app.route('/post/')
+def get_single_post():
+    return render_template('post-detail.html')
 
 # routes from other files:
 posts.initialize_routes(api)
